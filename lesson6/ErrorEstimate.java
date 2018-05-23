a9a273a540168d601030c3891e21dba799e7b03a
Kawamura Shintaro <kawasin73@gmail.com>
2018-05-23 21:36:46 +0900

    Done lesson6

--- lesson6/ErrorEstimate.java
+++ lesson6/ErrorEstimate.java
@@ -4,48 +4,76 @@ import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 
 public class ErrorEstimate {
-	public static void main(String args[]){
-		try{
-			BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("input points' interval h:");
-			System.out.flush();
-			double h=Double.parseDouble(br.readLine());
+    public static void main(String args[]) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("input points' interval h:");
+            System.out.flush();
+            double h = Double.parseDouble(br.readLine());
 
-			System.out.print("input x0(-h<=x0<=h):");
-			System.out.flush();
-			double x0=Double.parseDouble(br.readLine());
-			
-			System.out.print("input x1(-h<=x0<x1<=h):");
-			System.out.flush();
-			double x1=Double.parseDouble(br.readLine());
-			
+            System.out.print("input x0(-h<=x0<=h):");
+            System.out.flush();
+            double x0 = Double.parseDouble(br.readLine());
 
-			/*
-		
-			ここを編集してください
+            System.out.print("input x1(-h<=x0<x1<=h):");
+            System.out.flush();
+            double x1 = Double.parseDouble(br.readLine());
 
-			*/
+            System.out.println(String.format("The absolute value of maximum error is: %.10f", MaxErrorEstimate(h)));
+            for (double x = x0; x <= x1; x += h / 10.0) {
+                System.out.println(String.format("error(%.10f)=%.10f", x, ErrorEstimateCalc(h, x)));
+            }
+        } catch (IOException e) {
+            System.out.println("Error:" + e);
+        }
+    }
 
+    static double LagrangeCalc(double[] x, double[] f, double xint) {
+        // x[]: x座標   f[]: 関数値   xint: 補間点
+        double fint = 0.0;
+        for (int i = 0; i < x.length; i++) {
+            double l = 1.0;
+            for (int j = 0; j < x.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                l *= (xint - x[j]) / (x[i] - x[j]);
+            }
+            fint += (l * f[i]);
+        }
+        return fint;
+    }
 
-		}catch (IOException e){
-			System.out.println("Error:"+e);
-		}
-	}
-		
-	static double ErrorEstimateCalc(double h,double x){
-		/*
-		
-		ここを編集してください
+    static double ErrorEstimateCalc(double h, double xi) {
+        double x[] = new double[3];
+        double f[] = new double[3];
+        x[0] = -h;
+        x[1] = 0.0;
+        x[2] = h;
+        f[0] = Math.sin(-h);
+        f[1] = Math.sin(0);
+        f[2] = Math.sin(h);
+        return Math.sin(xi) - LagrangeCalc(x, f, xi);
+    }
 
-		*/
-	}
+    static double MaxErrorEstimate(double h) {
+        double x[] = new double[3];
+        x[0] = -h;
+        x[1] = 0.0;
+        x[2] = h;
 
-	static double MaxErrorEstimate(double h){
-		/*
-		
-		ここを編集してください
+        double b = (x[0] + x[1] + x[2]);
+        double target1 = -b + Math.sqrt(b * b - 3.0 * (x[0] * x[1] + x[1] * x[2] + x[0] * x[2])) / 3.0;
+        double target2 = -b - Math.sqrt(b * b - 3.0 * (x[0] * x[1] + x[1] * x[2] + x[0] * x[2])) / 3.0;
 
-		*/
-	}
+        double f1 = (target1 - x[0]) * (target1 - x[1]) * (target1 - x[2]) / 6;
+        double f2 = (target2 - x[0]) * (target2 - x[1]) * (target2 - x[2]) / 6;
+
+        if (f2 > f1) {
+            f1 = f2;
+        }
+
+        return f1;
+    }
 
 }
--- lesson6/Lagrange.java
+++ lesson6/Lagrange.java
@@ -3,54 +3,60 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 
-public class Lagrange{
-	static double LagrangeCalc(double[] x, double[] f, double xint){
-		// x[]: x座標   f[]: 関数値   xint: 補間点
-		/*
-		
-		ここを編集してください
+public class Lagrange {
+    static double LagrangeCalc(double[] x, double[] f, double xint) {
+        // x[]: x座標   f[]: 関数値   xint: 補間点
+        double fint = 0.0;
+        for (int i = 0; i < x.length; i++) {
+            double l = 1.0;
+            for (int j = 0; j < x.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                l *= (xint - x[j]) / (x[i] - x[j]);
+            }
+            fint += (l * f[i]);
+        }
+        return fint;
+    }
 
-		*/
-	}
-
-	public static void main(String agrs[]){
-		try{
-			BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("input the number of known points:");
-			System.out.flush();
-			int n=Integer.parseInt(br.readLine());
-            int flag=0;
-            while(n<2&&flag==0){
+    public static void main(String agrs[]) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("input the number of known points:");
+            System.out.flush();
+            int n = Integer.parseInt(br.readLine());
+            int flag = 0;
+            while (n < 2 && flag == 0) {
                 System.out.print("elligal input, please input again(input -1 to exit): ");
                 System.out.flush();
-                n=Integer.parseInt(br.readLine());
-                if(n==-1){
-                    flag=1;
+                n = Integer.parseInt(br.readLine());
+                if (n == -1) {
+                    flag = 1;
                 }
             }
-            
-            if(flag==0){
-			double x[]=new double[n];
-			double f[]=new double[n];
-			
-			for(int i=0; i<n; i++){
-				System.out.print("input No."+i+" x:");
-				System.out.flush();
-				x[i]=Double.parseDouble(br.readLine());
-				System.out.print("input No."+i+" f(x):");
-				System.out.flush();
-				f[i]=Double.parseDouble(br.readLine());
-			}
 
-			/*
-		
-			ここを編集してください
+            if (flag == 0) {
+                double x[] = new double[n];
+                double f[] = new double[n];
 
-			*/
+                for (int i = 0; i < n; i++) {
+                    System.out.print("input No." + i + " x:");
+                    System.out.flush();
+                    x[i] = Double.parseDouble(br.readLine());
+                    System.out.print("input No." + i + " f(x):");
+                    System.out.flush();
+                    f[i] = Double.parseDouble(br.readLine());
+                }
+
+                for (double xint = 1.0; xint <= 5.0; xint += 0.1) {
+                    double fint = LagrangeCalc(x, f, xint);
+                    System.out.println(String.format("(%f, %f)", xint, fint));
+                }
 
             }
-		}catch (IOException e){
-			System.out.println("Error:"+e);
-		}
-	}
+        } catch (IOException e) {
+            System.out.println("Error:" + e);
+        }
+    }
 }
