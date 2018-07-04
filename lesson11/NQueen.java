a432b3a1c2b151b8455a3162ef30c83973528ff3
Kawamura Shintaro <kawasin73@gmail.com>
2018-07-04 16:00:13 +0900

    Done lesson11

--- lesson11/NQueen.java
+++ lesson11/NQueen.java
@@ -1,41 +1,113 @@
 import java.util.*;
 
 public class NQueen {
-	private enum Status {
-		FREE,
-		NOT_FREE
-	}
+    private enum Status {
+        FREE,
+        NOT_FREE
+    }
 
-	private final int N;	// クイーンの数
-	private int[] pos;		// 各行の置かれたクイーンの位置
-	private Status[] col;	// クイーンが垂直方向に利いているかを示す配列
-	private Status[] down;	// クイーンが右斜め下向きに利いているかを示す配列
-	private Status[] up;	// クイーンが右斜め上向きに利いているかを示す配列
-	private int count;		// 解の数
+    private final int N;    // クイーンの数
+    private int[] pos;        // 各行の置かれたクイーンの位置
+    private Status[] col;    // クイーンが垂直方向に利いているかを示す配列
+    private Status[] down;    // クイーンが右斜め下向きに利いているかを示す配列
+    private Status[] up;    // クイーンが右斜め上向きに利いているかを示す配列
+    private int count;        // 解の数
 
-	// Nクイーン問題を解くためのオブジェクトを生成する
-	public NQueen(int numberOfQueen) {
-		// 配列を割り当てる
-		// クイーンの位置と利き筋を初期化する
-	}
+    // Nクイーン問題を解くためのオブジェクトを生成する
+    public NQueen(int numberOfQueen) {
+        // 配列を割り当てる
+        // クイーンの位置と利き筋を初期化する
+        this.N = numberOfQueen;
+        this.pos = new int[numberOfQueen];
+        this.col = new Status[numberOfQueen];
+        this.down = new Status[2 * numberOfQueen - 1];
+        this.up = new Status[2 * numberOfQueen - 1];
+        this.count = 0;
+        this.reset();
+    }
 
-	public boolean tryQueen(int a) {
-		// 行a以降のすべての行にクイーンを置く
-		// 左から右に向かって順番にクイーンが置けるかどうかを調べる
-		// 行aのb列目に置けるかどうかを,利き筋の情報を元に調べる
-		// 置くことができたら．場所を記録して，利き筋をセットする．
-		// クイーンが置けたらtrue,置けなかったらfalseを返す
-		// 行a+1以降のすべての行に置けたら成功.
-		// N個のクイーンをすべて置くことができれば成功．
-	}
+    public boolean tryQueen(int a) {
+        // 行a以降のすべての行にクイーンを置く
+        // 左から右に向かって順番にクイーンが置けるかどうかを調べる
+        // 行aのb列目に置けるかどうかを,利き筋の情報を元に調べる
+        // 置くことができたら．場所を記録して，利き筋をセットする．
+        // クイーンが置けたらtrue,置けなかったらfalseを返す
+        // 行a+1以降のすべての行に置けたら成功.
+        // N個のクイーンをすべて置くことができれば成功．
+        for (int b = 0; b < N; b++) {
+            if (col[b] == Status.NOT_FREE ||
+                    down[a - b + N - 1] == Status.NOT_FREE ||
+                    up[a + b] == Status.NOT_FREE) {
+                continue;
+            }
+            pos[a] = b;
+            col[b] = Status.NOT_FREE;
+            down[a - b + N - 1] = Status.NOT_FREE;
+            up[a + b] = Status.NOT_FREE;
+            if (a >= N - 1) {
+                return true;
+            }
+            if (tryQueen(a + 1)) {
+                return true;
+            }
+            pos[a] = -1;
+            col[b] = Status.FREE;
+            down[a - b + N - 1] = Status.FREE;
+            up[a + b] = Status.FREE;
+        }
+        return false;
+    }
 
-	// クイーンの位置を出力する
-	public void print() {
-		// 表示形式に沿って出力してください
-	}
+    // クイーンの位置を出力する
+    public void print() {
+        // 表示形式に沿って出力してください
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (pos[i] == j) {
+                    System.out.print("Q");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
 
-	public void tryQueenAll(int a) {
-		// ここに処理を書きます
-	}
+    private void reset() {
+        Arrays.fill(pos, -1);
+        Arrays.fill(col, Status.FREE);
+        Arrays.fill(down, Status.FREE);
+        Arrays.fill(up, Status.FREE);
+    }
+
+    public void tryQueenAll(int a) {
+        // ここに処理を書きます
+
+        for (int b = 0; b < N; b++) {
+            if (col[b] == Status.NOT_FREE ||
+                    down[a - b + N - 1] == Status.NOT_FREE ||
+                    up[a + b] == Status.NOT_FREE) {
+                continue;
+            }
+            pos[a] = b;
+            col[b] = Status.NOT_FREE;
+            down[a - b + N - 1] = Status.NOT_FREE;
+            up[a + b] = Status.NOT_FREE;
+            if (a >= N - 1) {
+                print();
+                System.out.println("=========================================");
+                count++;
+            } else {
+                tryQueenAll(a + 1);
+            }
+            pos[a] = -1;
+            col[b] = Status.FREE;
+            down[a - b + N - 1] = Status.FREE;
+            up[a + b] = Status.FREE;
+        }
+        if (a == 0) {
+            System.out.println(String.format("Total: %d", count));
+        }
+    }
 
 }
--- lesson11/NQueenMain.java
+++ lesson11/NQueenMain.java
@@ -1,32 +1,36 @@
 public class NQueenMain {
 
-	 public static void abort(String msg) {
+    public static void abort(String msg) {
         System.err.println("Usage: java NQueenMain <queens>");
         System.err.println(msg);
         System.exit(1);
     }
 
-	 public static void main(String[] args) {
-	 	if (args.length != 1){
-	 		abort("Invalid length of args");
-	 		return;
-	 	}
-	 	int n = 0;
-	 	try {
-	 		n = Integer.parseInt(args[0]);
-	 	} catch(NumberFormatException e) {
-	 		abort("Invalid # of queens: " + args[0]);
-	    }
-	 	if (n<=0) {
-	 		abort("Invalid # of queens: " + args[0]);
-	 	}
+    public static void main(String[] args) {
+        if (args.length > 2) {
+            abort("Invalid length of args");
+            return;
+        }
+        int n = 0;
+        try {
+            n = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            abort("Invalid # of queens: " + args[0]);
+        }
+        if (n <= 0) {
+            abort("Invalid # of queens: " + args[0]);
+        }
 
-	 	NQueen nq = new NQueen(n);
+        NQueen nq = new NQueen(n);
 
-	 	if (nq.tryQueen(0)) {
-	 		nq.print();
-	 	} else {
-	 		System.out.println("No solution.");
-	 	}
-	}
+        if (args.length == 2 && args[1].equals("all")) {
+            nq.tryQueenAll(0);
+            return;
+        }
+        if (nq.tryQueen(0)) {
+            nq.print();
+        } else {
+            System.out.println("No solution.");
+        }
+    }
 }
